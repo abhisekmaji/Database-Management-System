d810b7075a17a59b6ad38249d9f7dba487ee22ee
abhisekmaji <46377796+abhisekmaji@users.noreply.github.com>
2021-03-07 03:58:01 +0530

    Add files via upload

--- ass2/query.sql
+++ ass2/query.sql
@@ -1,16 +1,30 @@
 --PREAMBLE--
 create view authorconnected as
-    select apl.authorid as author1 , apl2.authorid as author2 , apl1.paperid
+    select distinct apl1.authorid as author1 , apl2.authorid as author2 , apl1.paperid
     from authorpaperlist as apl1 join authorpaperlist as apl2
         on apl1.paperid = apl2.paperid
-    where apl1.authorid <> apl2.authorid
+    where apl1.authorid <> apl2.authorid;
 
 create view allpair as 
     select ad1.authorid as author1 , ad2.authorid as author2 , 
         ad1.authorname as author1name, ad2.authorname as author2name
     from authordetails as ad1 , authordetails as ad2
-    where ad1.authorid <> ad2.authorid
+    where ad1.authorid <> ad2.authorid;
 
+create view allcit as 
+select distinct *
+from
+    (with recursive allcitations (paper1, paper2) as(
+            select paperid1, paperid2
+            from citationlist
+        union
+            select c.paper1 , cl.paperid2
+            from allcitations as c join citationlist as cl 
+                    on c.paper2 = cl.paperid1
+        )
+        select *
+        from allcitations) as table_;
+/*
 --1--
 with recursive reachable (origin , dest, carrier) as(
         select originairportid, destairportid, carrier
@@ -68,9 +82,9 @@ with recursive longest (origin , path_, length_ , dest) as(
             on r1.dest = flights.originairportid
         where flights.originairportid <> all(r1.path_)
     )
-    select length_
+    select length_ as length
     from longest
-    where 10140 = some(longest.path_) and dest = origin
+    where origin = dest and origin = 10140
     order by length_ desc
     limit 1;    
 
@@ -84,7 +98,7 @@ with recursive longest (origin , path_, length_ , dest) as(
             on r1.dest = flights.originairportid
         where flights.originairportid <> all(r1.path_)
     )
-    select length_
+    select length_ as length
     from longest
     where dest = origin
     order by length_ desc
@@ -97,7 +111,7 @@ with recursive numpaths (origin_city , path_, dest_city) as(
                 flights.originairportid = air1.airportid
                 join airports as air2 on 
                 flights.destairportid = air2.airportid
-        where air1.name = 'Albuquerque'
+        where air1.city = 'Albuquerque'
                 and air1.state <> air2.state
     union
         select np.origin_city, np.path_ || air1.city as path_, air2.city
@@ -121,7 +135,7 @@ with recursive numpaths (origin_city , path_, dest_city) as(
                 flights.originairportid = air1.airportid
                 join airports as air2 on 
                 flights.destairportid = air2.airportid
-        where air1.name = 'Albuquerque'
+        where air1.city = 'Albuquerque'
     union
         select np.origin_city, np.path_ || air1.city as path_, air2.city
         from flights join airports as air1
@@ -138,25 +152,9 @@ with recursive numpaths (origin_city , path_, dest_city) as(
 
 --8--
 (   
-    (select city1.city as name1, city2.city as name2
-    from
-        (select city
-        from flights join airports 
-                on originairportid = airportid
-        union
-        select city
-        from flights join airports 
-                on originairportid = airportid
-        )as city1,
-        (select city
-        from flights join airports 
-                on originairportid = airportid
-        union
-        select city
-        from flights join airports 
-                on originairportid = airportid
-        )as city2
-    where city1.city <> city2.city)
+    (select air1.city as name1, air2.city as name2
+    from airports as air1 , airports as air2
+    where air1.airportid <> air2.airportid)
 
     EXCEPT
 
@@ -215,39 +213,209 @@ with recursive incdelay (origin_city, dest_city, delay_) as(
                     join incdelay on incdelay.dest_city = a1.city
         where (flights.departuredelay + flights.arrivaldelay) >= incdelay.delay_
     )
-    select origin_city as name1, dest_city as name2
+    select distinct origin_city as name1, dest_city as name2
     from incdelay
-    where origin_city <> dest_city
-    order by origin_city, dest_city;
-
+    order by name1, name2;
+*/
 --12--
 with recursive AtoB (path_ , depth_ , author) as( 
         select ARRAY[author1] as path_ , 1 as depth_ , author2
-        from authorconnected as ac join authordetails as ad
-                on ac.author1 = ad.authorid
-        where ad.authorname = 'A'
+        from authorconnected as ac
+        where author1 = 1235
     union
-        select p.path_ || ac.author2 , p.depth_ + 1 as depth_, p.author2
+        select p.path_ || ac.author1 , p.depth_ + 1 as depth_, ac.author2
         from authorconnected as ac join AtoB as p
             on p.author = ac.author1
         where p.author <> all(p.path_)
     )
     (
-        (select author as authorid , depth_ as count
-        from AtoB
+        (select authorid , count
+        from
+            (select author as authorid , depth_ as count , 
+                row_number() over(partition by author order by depth_) as rnk
+            from AtoB
+            where author != 1235
+            )as table1
+        where rnk = 1
         )
     union
         (select ap.author2 as authorid, -1 as count
         from allpair as ap
-        where ap.author1name = 'A' and ap.author2 not in    (select author
+        where ap.author1 = 1235 and ap.author2 not in    (select author
                                                             from AtoB
                                                             )
         )
     )
     order by count DESC , authorid;
 
---13--
+--13-- 
+with recursive 
+    AtoB (author1, path_ , depth_ , gender, author2) as( 
+        select author1 , ARRAY[author1] as path_ , 1 as depth_ , '' as gender , author2
+        from authorconnected as ac
+        where author1 = 1558
+    union
+        select p.author1, p.path_ || ac.author1 , p.depth_ + 1 as depth_, ad1.gender , ac.author2
+        from authorconnected as ac 
+            join AtoB as p on p.author2 = ac.author1
+            join authordetails as ad1 on ad1.authorid = p.author2
+            join authordetails as ad2 on ad2.authorid = ac.author2
+        where ((p.depth_ = 1 and ad1.age > 35 ) or ( p.depth_ >=2 and ad1.age > 35 and ad1.gender != p.gender )) and ac.author1 <> all(p.path_)
+    ),
+    componentsA (author1, author2) as(
+        select author1, author2
+        from authorconnected as ac
+        where author1 = 1558
+    union
+        select ca.author1 , ac.author2
+        from authorconnected as ac
+            join componentsA as ca on ca.author2 = ac.author1
+    )
+    select
+    case
+        when 2826 in (  select author2
+                        from componentsA) 
+            then (select
+                    case
+                    when 2826 in (select author2 from AtoB) then (select count(distinct path_) as count
+                                                                from AtoB
+                                                                where author2 = 2826
+                                                                GROUP by author2 )
+                    else 0
+                    end as count)
+        else -1
+    end as count; 
 
+
+--14--
+/*with recursive components (paper1, paper2) as(
+        select paperid1, paperid2
+        from citationlist
+    union
+        select c.paper1 , cl.paperid2
+        from components as c join citationlist as cl 
+                on c.paper2 = cl.paperid1
+    )
+    select *
+    from components;*/
+
+--18--
+with recursive 
+    AtoB (author1, path_ , depth_ , author2) as( 
+        select author1 , ARRAY[author1] as path_ , 1 as depth_ , author2
+        from authorconnected as ac
+        where author1 = 3552
+    union
+        select p.author1, p.path_ || ac.author1 , p.depth_ + 1 as depth_, ac.author2
+        from authorconnected as ac 
+            join AtoB as p on p.author2 = ac.author1
+            join authordetails as ad1 on ad1.authorid = p.author2
+            join authordetails as ad2 on ad2.authorid = ac.author2
+        where ac.author1 <> all(p.path_)
+    )
+    select
+    case
+        when 321 in (  select author2
+                        from AtoB) 
+            then (  select count(distinct path_) as count
+                    from AtoB
+                    where (1436 = some(path_) 
+                        or 562 = some(path_)
+                        or 921 = some(path_))
+                        and author2 = 321
+                    GROUP by author2
+                 )
+        else -1
+    end as count;
+
+--19--
+with recursive 
+    AtoB (author1, path_ , depth_ , city_, paper_, author2) as( 
+        select author1 , ARRAY[author1] as path_ , 1 as depth_ ,
+             array[''] as city_ ,array[-1] as paper_, author2
+        from authorconnected as ac
+        where author1 = 3552
+    union
+        select p.author1, p.path_ || ac.author1 , p.depth_ + 1 as depth_,
+            p.city_ || ad1.city , p.paper_ || apd.paperid , ac.author2
+        from authorconnected as ac 
+            join AtoB as p on p.author2 = ac.author1
+            join authordetails as ad1 on ad1.authorid = p.author2
+            join authorpaperlist as apd on apd.authorid = p.author2
+            join citationlist as cl on cl.paperid1 = apd.paperid
+        where ((p.depth_ = 1 ) or ( p.depth_ >=2 and ad1.city <> all(p.city_) and cl.paperid2 <> all(p.paper_))) 
+            and ac.author1 <> all(p.path_)
+    ),
+    componentsA (author1, author2) as(
+        select author1, author2
+        from authorconnected as ac
+        where author1 = 3552
+    union
+        select ca.author1 , ac.author2
+        from authorconnected as ac
+            join componentsA as ca on ca.author2 = ac.author1
+    )
+    /*select distinct *
+    from AtoB
+    where author2 = 321;
+    */select
+    case
+        when 321 in (  select author2
+                        from componentsA) 
+            then (select
+                    case
+                    when 321 in (select author2 from AtoB) then (select count(distinct path_) as count
+                                                                from AtoB
+                                                                where author2 = 321
+                                                                GROUP by author2 )
+                    else 0
+                    end as count)
+        else -1
+    end as count;
+
+--20--
+with recursive 
+    AtoB (author1, path_ , depth_ , city_, paper_, author2) as( 
+        select author1 , ARRAY[author1] as path_ , 1 as depth_ ,
+             array[''] as city_ ,array[-1] as paper_, author2
+        from authorconnected as ac
+        where author1 = 3552
+    union
+        select p.author1, p.path_ || ac.author1 , p.depth_ + 1 as depth_,
+            p.city_ || ad1.city , p.paper_ || apd.paperid , ac.author2
+        from authorconnected as ac 
+            join AtoB as p on p.author2 = ac.author1
+            join authordetails as ad1 on ad1.authorid = p.author2
+            join authorpaperlist as apd on apd.authorid = p.author2
+            join allcit as act on act.paper1 = apd.paperid
+        where ((p.depth_ = 1 ) or ( p.depth_ >=2 and ad1.city <> all(p.city_) and act.paper2 <> all(p.paper_))) 
+            and ac.author1 <> all(p.path_)
+    ),
+    componentsA (author1, author2) as(
+        select author1, author2
+        from authorconnected as ac
+        where author1 = 3552
+    union
+        select ca.author1 , ac.author2
+        from authorconnected as ac
+            join componentsA as ca on ca.author2 = ac.author1
+    )
+    select
+    case
+        when 321 in (  select author2
+                        from componentsA) 
+            then (select
+                    case
+                    when 321 in (select author2 from AtoB) then (select count(distinct path_) as count
+                                                                from AtoB
+                                                                where author2 = 321
+                                                                GROUP by author2 )
+                    else 0
+                    end as count)
+        else -1
+    end as count;
 
 --CLEANUP--
-drop view authorconnected
+drop view authorconnected;
+drop view allpair;
+drop view allcit;
