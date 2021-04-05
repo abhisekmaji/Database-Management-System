5309d567236f92dea8f3a16cab4f448485537b94
Abhisek Maji <46377796+abhisekmaji@users.noreply.github.com>
2021-04-05 18:29:39 +0530

    Delete query5.sql

--- project/query5.sql
+++
@@ -1,5 +0,0 @@
-select acquiring_object_id, acquired_object_id, term_code, price_amount, price_currency_code, acquired_at
-from acquisitions
-where (acquiring_object_id = 'x' and acquired_object_id = 'y') 
-    or (acquiring_object_id = 'y' and acquired_object_id = 'x')
-order by acquired_at asc;
