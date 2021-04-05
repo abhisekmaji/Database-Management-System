bc4442246c2859e51d1593a625b8f25b3eae8a65
Abhisek Maji <46377796+abhisekmaji@users.noreply.github.com>
2021-04-05 18:29:20 +0530

    Delete query3.sql

--- project/query3.sql
+++
@@ -1,8 +0,0 @@
-select relationship_object_id as company_name, title as role , is_past as current_job_status
-from relationships
-where person_object_id = 'x' and relationship_object_id like 'c%'
-order by relationship_object_id desc, title desc;
-
-select first_name, last_name, birthplace, affliation_name 
-from people
-where object_id = 'x';
