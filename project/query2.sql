2533cebc39b573745baa5820a6a4fc40b6e91040
Abhisek Maji <46377796+abhisekmaji@users.noreply.github.com>
2021-04-05 18:29:11 +0530

    Delete query2.sql

--- project/query2.sql
+++
@@ -1,9 +0,0 @@
-select funded_object_id
-from investments
-where investor_object_id = 'x'
-order by funded_object_id desc;
-
-select distinct funded_object_id
-from investments
-where investor_object_id = 'x'
-order by funded_object_id desc;
